aa3a347a11ebd6717774af7a6113d5a2db2dd517
Al_sakhen <ahmadalsakhen36@gmail.com>
2021-09-06 00:23:24 +0300

    swipper updated

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,14 +1,14 @@
 <!-- ======= Footer ======= -->
 <footer id="footer" >
 
-      <div class="container py-4">
+        <div class="container py-4">
         <div class="copyright">
-          &copy; Copyright <strong><span>DEEK</span></strong>. All Rights Reserved
+            &copy; Copyright <strong><span>CleverTeam</span></strong>. All Rights Reserved
         </div>
-        <div class="credits">
-          Designed by <a href="http://deek.codes">Abdulkareem Aldeek</a>
+        {{-- <div class="credits">
+            Designed by <a href="http://deek.codes">Abdulkareem Aldeek</a>
+        </div> --}}
         </div>
-      </div>
 </footer><!-- End Footer -->
 
     <div id="preloader"></div>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -228,68 +228,37 @@
 
                     <div class="swiper-slide">
                         <div class="testimonial-item">
-                            <img src="{{asset('assets/img/testimonials/testimonials-1.jpg')}}" class="testimonial-img" alt="">
-                            <h3>Saul Goodman</h3>
-                            <h4>Ceo &amp; Founder</h4>
+                            <img src="{{asset('assets/img/testimonials/mohammad.jpg')}}" class="testimonial-img" alt="">
+                            <h3>Mohammed Alqudah</h3>
                             <p>
                                 <i class="bx bxs-quote-alt-left quote-icon-left"></i>
-                                Proin iaculis purus consequat sem cure digni ssim donec porttitora entum suscipit rhoncus. Accusantium quam, ultricies eget id, aliquam eget nibh et. Maecen aliquam, risus at semper.
-                                <i class="bx bxs-quote-alt-right quote-icon-right"></i>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex aliquam illo non assumenda dolore eaque ipsum, architecto veritatis obcaecati sunt! Dolores eos amet beatae expedita vero tempora molestias minima labore.                                <i class="bx bxs-quote-alt-right quote-icon-right"></i>
                             </p>
                         </div>
                     </div><!-- End testimonial item -->
 
                     <div class="swiper-slide">
                         <div class="testimonial-item">
-                            <img src="{{asset('assets/img/testimonials/testimonials-2.jpg')}}" class="testimonial-img" alt="">
-                            <h3>Sara Wilsson</h3>
-                            <h4>Designer</h4>
+                            <img src="{{asset('assets/img/testimonials/abd.jpg')}}" class="testimonial-img" alt="">
+                            <h3>Abdulkareem Aldeek</h3>
                             <p>
                                 <i class="bx bxs-quote-alt-left quote-icon-left"></i>
-                                Export tempor illum tamen malis malis eram quae irure esse labore quem cillum quid cillum eram malis quorum velit fore eram velit sunt aliqua noster fugiat irure amet legam anim culpa.
-                                <i class="bx bxs-quote-alt-right quote-icon-right"></i>
+                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Labore, ab corrupti. Eaque neque earum voluptatibus at asperiores quaerat laboriosam cupiditate vero fugit ipsum eveniet laborum excepturi, ea nesciunt quam? Eum.                                <i class="bx bxs-quote-alt-right quote-icon-right"></i>
                             </p>
                         </div>
                     </div><!-- End testimonial item -->
 
                     <div class="swiper-slide">
                         <div class="testimonial-item">
-                            <img src="{{asset('assets/img/testimonials/testimonials-3.jpg')}}" class="testimonial-img" alt="">
-                            <h3>Jena Karlis</h3>
-                            <h4>Store Owner</h4>
+                            <img src="{{asset('assets/img/testimonials/ahmad.jpg')}}" class="testimonial-img" alt="">
+                            <h3>Ahmad Alsakhen</h3>
                             <p>
                                 <i class="bx bxs-quote-alt-left quote-icon-left"></i>
-                                Enim nisi quem export duis labore cillum quae magna enim sint quorum nulla quem veniam duis minim tempor labore quem eram duis noster aute amet eram fore quis sint minim.
-                                <i class="bx bxs-quote-alt-right quote-icon-right"></i>
+                                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas dolorum iure eius accusamus et dignissimos neque eligendi, fuga dolor suscipit fugit ullam, qui labore porro dolore magnam harum temporibus consectetur!                                <i class="bx bxs-quote-alt-right quote-icon-right"></i>
                             </p>
                         </div>
                     </div><!-- End testimonial item -->
 
-                    <div class="swiper-slide">
-                        <div class="testimonial-item">
-                            <img src="{{asset('assets/img/testimonials/testimonials-4.jpg')}}" class="testimonial-img" alt="">
-                            <h3>Matt Brandon</h3>
-                            <h4>Freelancer</h4>
-                            <p>
-                                <i class="bx bxs-quote-alt-left quote-icon-left"></i>
-                                Fugiat enim eram quae cillum dolore dolor amet nulla culpa multos export minim fugiat minim velit minim dolor enim duis veniam ipsum anim magna sunt elit fore quem dolore labore illum veniam.
-                                <i class="bx bxs-quote-alt-right quote-icon-right"></i>
-                            </p>
-                        </div>
-                    </div><!-- End testimonial item -->
-
-                    <div class="swiper-slide">
-                        <div class="testimonial-item">
-                            <img src="{{asset('assets/img/testimonials/testimonials-5.jpg')}}" class="testimonial-img" alt="">
-                            <h3>John Larson</h3>
-                            <h4>Entrepreneur</h4>
-                            <p>
-                                <i class="bx bxs-quote-alt-left quote-icon-left"></i>
-                                Quis quorum aliqua sint quem legam fore sunt eram irure aliqua veniam tempor noster veniam enim culpa labore duis sunt culpa nulla illum cillum fugiat legam esse veniam culpa fore nisi cillum quid.
-                                <i class="bx bxs-quote-alt-right quote-icon-right"></i>
-                            </p>
-                        </div>
-                    </div><!-- End testimonial item -->
 
                 </div>
                 <div class="swiper-pagination"></div>
